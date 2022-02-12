3de3ff09287b3dfc85c924a53a640acab011b197
Alice <purpleplayer@yandex.ru>
2022-02-13 00:54:59 +0300

    task_1 | working non efficient solution with two consecutive loops

--- task_1/headers/brackets.hpp
+++ task_1/headers/brackets.hpp
@@ -3,6 +3,10 @@
 #ifndef BRACKETS_HPP
 # define BRACKETS_HPP
 
+# define EMPTY 0
+# define UNIQUE 1
+# define REPEATED 2
+
 #include <iostream>
 
 #endif
--- task_1/sources/brackets.cpp
+++ task_1/sources/brackets.cpp
@@ -2,6 +2,27 @@
 
 std::string&	brackets(std::string& str)
 {
+	int ascii_set[256] = { EMPTY };
+	int ascii_char;
+
+	for (size_t i = 0; i < str.length(); i++)
+	{
+		ascii_char = std::tolower(static_cast<int>(str[i]));
+		if (ascii_set[ascii_char] == EMPTY)
+			ascii_set[ascii_char] = UNIQUE;
+		else
+			ascii_set[ascii_char] = REPEATED;
+	}
+
+	for (size_t i = 0; i < str.length(); i++)
+	{
+		ascii_char = std::tolower(static_cast<int>(str[i]));
+		if (ascii_set[ascii_char] == UNIQUE)
+			str[i] = '(';
+		else
+			str[i] = ')';
+	}
+	
 	return str;
 }
 
@@ -14,5 +35,6 @@ int	main(int argc, char** argv)
 		std::string src = argv[1];
 		std::cout << brackets(src) << std::endl;
 	}
+
 	return 0;
 }
